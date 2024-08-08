24275b85b09a723440eacd67b7372492893da366
IshkhanB <b2068666@gmail.com>
2024-08-08 20:09:45 +0300

    080824

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+
+<template>
+<h1 @click="str+='!'">{{ str }} 
+</h1>
+</template>
+
+<script setup lang="ts">
+import {ref} from 'vue'
+const str = ref('learn vue')
+</script>
+
+<style scoped>
+h1{
+  user-select: none;
+}
+</style>
